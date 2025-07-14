8a588c85f463a971f2d4c8d5582cb02274ad0cfd
Ming Thanyawet <67321630+MingNarongthat@users.noreply.github.com>
2025-07-15 00:19:53 +0700

    Update Header.vue

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -7,14 +7,14 @@
     <nav class="flex items-center space-x-4">
       <NuxtLink to="/" class="mx-2 text-brand-dark hover:text-brand-primary font-medium">Home</NuxtLink>
       <NuxtLink to="/about" class="mx-2 text-brand-dark hover:text-brand-primary font-medium">About</NuxtLink>
-      <NuxtLink to="/contact" class="mx-2 text-brand-dark hover:text-brand-primary font-medium">Contact</NuxtLink>
       <NuxtLink to="/articles" class="mx-2 text-brand-dark hover:text-brand-primary font-medium">Articles</NuxtLink>
-      <NuxtLink 
+      <NuxtLink to="/contact" class="mx-2 text-brand-dark hover:text-brand-primary font-medium">Contact</NuxtLink>
+      <!-- <NuxtLink 
         to="/admin/login" 
         class="bg-brand-primary text-brand-dark px-4 py-2 rounded-md hover:bg-brand-light transition duration-200 ml-4 font-semibold"
       >
         Login
-      </NuxtLink>
+      </NuxtLink> -->
     </nav>
   </header>
 </template> 
